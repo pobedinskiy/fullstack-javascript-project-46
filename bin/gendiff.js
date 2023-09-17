0025acf0306efbd4b32f39f8924ad205e500179b
pobedinskiy <dennis8860@gmail.com>
2023-09-17 21:57:41 +0300

    step 2 - change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference')
   .version('0.0.1', '-V, --VERSION', 'output the version number')
-  .helpOption('-h, --help', 'output the version number');
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output the version number')
+  .option('-f, --format <type>', 'output format');
   
 program.parse();
